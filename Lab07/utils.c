adf0ca1c9c1742af368a7e928b78fd6e1412a38c
Georges034302 <georges.boughantous@gmail.com>
2021-04-21 17:33:48 +1000

    lab7

--- Lab07/utils.c
+++ Lab07/utils.c
@@ -13,45 +13,111 @@ int readInt(char s[]){
     return value;
 }
 
-//Push a cell to the begining of a list
 void push(Node** head_ref, int key) {  
- 
+  Node* new_node = (struct cell*)malloc(sizeof(struct cell));  
+  new_node->key = key;
+  new_node->next = (*head_ref);
+  (*head_ref) = new_node;
 }
 
-//Traverse a list to the end then add an elment after the last
 void append(Node** head_ref, int key) {
- 
+  Node* new_node = (struct cell*)malloc(sizeof(struct cell));
+  Node* last = *head_ref;
+
+  new_node->key = key;
+  new_node->next = NULL;
+
+  if (*head_ref == NULL) {
+    *head_ref = new_node;
+    return;
+  }
+
+  while (last->next != NULL)
+    last = last->next;
+
+  last->next = new_node;
+  return;
 }
 
-//Create a list of random-key values
 void createList(Node **head_ref, int size,int min, int max){
-    
+    push(&*head_ref,random(min,max));
+    for(int i = 1; i< size; i++)
+        append(&*head_ref,random(min,max));
 }
 
-//Return a node by key
 Node *find(Node** head_ref, int key){
-    
-    return NULL;
+    Node *current = *head_ref;
+
+    if(current == NULL) return NULL;
+
+    while (current->key != key){
+      if(current->next == NULL) 
+         return NULL;
+      else          
+         current = current->next;      
+    }
+
+    return current;
 }
 
-//Insert a cell after a Node of value: key
 void insert(Node** head_ref, int key,int min, int max){
+    Node *current = find(&*head_ref,key);
+    if(current == NULL) {
+      printf("\nCurrent Node cannot be NULL, [%d,next] does not exist\n\n",key);
+      return;
+    }
    
+    Node* new_node = (struct cell*)malloc(sizeof(struct cell));
+    new_node->key  = random(min,max);
+    new_node->next = current->next;
+    current->next = new_node;
+     printf("\n [%d,next] has been added to the list\n\n",new_node->key);
 }
 
-//Delete a Node from a list by key
 void deleteNode(Node** head_ref, int key){   
-   
-}
+    Node *current = *head_ref;
+    Node* previous = NULL;
 
-//Print a linked list
-void printList(Node** head_ref) {
+    if (current != NULL && current->key == key){
+        *head_ref = current->next; 
+        printf("\n Head->[%d,next] has been deleted!\n\n",current->key);
+        free(current);            
+        return;
+    }else{
+      while (current != NULL && current->key != key){
+        previous = current;
+        current = current->next;
+      }
     
+      if (current == NULL) return;
+
+      previous->next = current->next;
+      printf("\n [%d,next] has been deleted!\n\n",current->key);
+      free(current);
+    }
 }
 
-//Delete a linked list
+void printList(Node** head_ref) {
+    Node *ptr = *head_ref; 
+    printf("\nHEAD->");       
+    while(ptr != NULL) {
+        printf("[%d,next]->",ptr->key);
+        ptr = ptr->next;
+    }    
+    printf("NULL\n\n");
+}
+
 void freeList(Node** head_ref){   
-  
+   Node* current = *head_ref;
+   Node* next;
+ 
+   while (current != NULL)   {
+       next = current->next;
+       free(current);
+       current = next;
+   } 
+   *head_ref = NULL;   
+   printf("\nThe linked list has been deleted!\n\n");
 }
 
 
